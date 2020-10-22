1ab9ca54b6b2bba99a0b044382d69a0d58333dad
lexx <sd1328@gmail.com>
2020-10-22 10:40:15 +0300

    fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,31 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * Class Controller
+ * @OA\Info(
+ *     title="Тест - Web Excel API",
+ *     version="1.0",
+ *     description="Тестовое задание - редактор таблиц онлайн"
+ * ),
+ *
+ * @OA\Get(
+ *     path="/",
+ *     description="Home page",
+ *     @OA\Response(response="default", description="Welcome page")
+ * )
+ *
+ * @OA\Schema(
+ *     schema="ValidationError",
+ *     @OA\Property(property="field", type="array", description="error", example={"You have some error by field"},
+ *          @OA\Items(
+ *              @OA\Schema(type="string", example="You have some error by field")
+ *          )
+ *      )
+ * ),
+ *
+ * @package App\Http\Controllers
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
