9d73de135debb085d7ddde3f45256eaaa68bc415
lexx <sd1328@gmail.com>
2020-10-26 17:36:32 +0200

    fix

--- resources/views/table/editor.blade.php
+++ resources/views/table/editor.blade.php
@@ -5,17 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>{{ $table->name }}</title>
-    <link href="/css/excel/app.5ff6851c.css" rel="preload" as="style">
+    <link href="/css/excel/app.0177e335.css" rel="preload" as="style">
     <link href="/css/excel/chunk-vendors.a2cadc9e.css" rel="preload" as="style">
-    <link href="/js/excel/app.be58c1bb.js" rel="preload" as="script">
+
+    <link href="/js/excel/app.ba425bfe.js" rel="preload" as="script">
     <link href="/js/excel/chunk-vendors.0cc3cb7d.js" rel="preload" as="script">
+
     <link href="/css/excel/chunk-vendors.a2cadc9e.css" rel="stylesheet">
-    <link href="/css/excel/app.5ff6851c.css" rel="stylesheet">
+    <link href="/css/excel/app.0177e335.css" rel="stylesheet">
 </head>
 <body>
 <div id="app">
 </div>
 <script src="/js/excel/chunk-vendors.0cc3cb7d.js"></script>
-<script src="/js/excel/app.be58c1bb.js"></script>
+<script src="/js/excel/app.ba425bfe.js"></script>
 </body>
 </html>
