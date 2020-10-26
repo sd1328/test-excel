924b7ee2455c41bcba8829df0192f268615edf2b
lexx <sd1328@gmail.com>
2020-10-26 17:06:01 +0200

    fix

--- resources/views/table/editor.blade.php
+++ resources/views/table/editor.blade.php
@@ -4,23 +4,18 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="icon" href="/favicon.ico">
-    <title>hello-world</title>
-    <link href="/app/css/app.14c312b0.css" rel="preload" as="style">
-    <link href="/app/css/chunk-vendors.a2cadc9e.css" rel="preload" as="style">
-    <link href="/app/js/app.43a2fa1e.js" rel="preload" as="script">
-    <link href="/app/js/chunk-vendors.9c29a023.js" rel="preload" as="script">
-    <link href="/app/css/chunk-vendors.a2cadc9e.css" rel="stylesheet">
-    <link href="/app/css/app.14c312b0.css" rel="stylesheet">
+    <title>{{ $table->name }}</title>
+    <link href="/css/excel/app.5ff6851c.css" rel="preload" as="style">
+    <link href="/css/excel/chunk-vendors.a2cadc9e.css" rel="preload" as="style">
+    <link href="/js/excel/app.be58c1bb.js" rel="preload" as="script">
+    <link href="/js/excel/chunk-vendors.0cc3cb7d.js" rel="preload" as="script">
+    <link href="/css/excel/chunk-vendors.a2cadc9e.css" rel="stylesheet">
+    <link href="/css/excel/app.5ff6851c.css" rel="stylesheet">
 </head>
 <body>
 <div id="app">
-
 </div>
-<script src="/app/js/chunk-vendors.9c29a023.js"></script>
-<script src="/app/js/app.43a2fa1e.js"></script>
+<script src="/js/excel/chunk-vendors.0cc3cb7d.js"></script>
+<script src="/js/excel/app.be58c1bb.js"></script>
 </body>
 </html>
-
-{{-- Так можно получить ID таблицы --}}
-{{-- $table->id --}}
