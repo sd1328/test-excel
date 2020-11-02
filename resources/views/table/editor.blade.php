efb2ae842b00e1be398ca2de29cbd03ec9b9b1b0
lexx <sd1328@gmail.com>
2020-11-02 11:08:39 +0200

    fix front

--- resources/views/table/editor.blade.php
+++ resources/views/table/editor.blade.php
@@ -5,19 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>{{ $table->name }}</title>
-    <link href="/css/excel/app.aa0bebd6.css" rel="preload" as="style">
+    <link href="/css/excel/app.de697f15.css" rel="preload" as="style">
     <link href="/css/excel/chunk-vendors.a2cadc9e.css" rel="preload" as="style">
 
-    <link href="/js/excel/app.e808d311.js" rel="preload" as="script">
+    <link href="/js/excel/app.508ac573.js" rel="preload" as="script">
     <link href="/js/excel/chunk-vendors.68981942.js" rel="preload" as="script">
 
     <link href="/css/excel/chunk-vendors.a2cadc9e.css" rel="stylesheet">
-    <link href="/css/excel/app.aa0bebd6.css" rel="stylesheet">
+    <link href="/css/excel/app.de697f15.css" rel="stylesheet">
 </head>
 <body>
 <div id="app">
 </div>
 <script src="/js/excel/chunk-vendors.68981942.js"></script>
-<script src="/js/excel/app.e808d311.js"></script>
+<script src="/js/excel/app.508ac573.js"></script>
 </body>
 </html>
